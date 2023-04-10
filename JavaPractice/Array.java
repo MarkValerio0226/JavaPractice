a4cdaccf23b645baa0188f074277484307dab6bf
MarkValerio0226 <130329207+MarkValerio0226@users.noreply.github.com>
2023-04-10 11:12:45 +0800

    Create Array.java

---
+++ JavaPractice/Array.java
@@ -0,0 +1,13 @@
+package JavaPractice;
+
+public class Array {
+    // array = use to store multiple values in single variable
+
+    public static void main(String[]args ){
+    String[] cars = {"Mark", "Car", "Apple", "Tesla"};
+                    // elements ^^
+    cars[0] = "Banana";
+
+    System.out.println(cars[2]);
+    }
+}
